9ee5ac8b07c0a1a145501fb0d3ad11cb58520e05
Phyo Pyae Sone <pphyo3469@gmail.com>
2020-04-23 13:36:27 +0630

    Initial commit

---
+++ C++/Projects/OOP Dr.Aung Win Htut lessons/OOP Dr.Aung Win Htut lessons/main.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+	//cout << "Hello World!";
+	//_getch();
+	//return 0;
+
+	//int var1;
+	//int var2;
+	//var1 = 20;
+	//var2 = var1 + 10;
+	//cout << "var1+10 is " << var2 << endl;
+	//return 0;
+
+	//char charvar1 = 'A';
+	//char charvar2 = '\t';
+	//cout << charvar1;
+	//cout << charvar2;
+	//charvar1 = 'B';
+	//cout << charvar1;
+	//cout << '\n';
+	//_getch();
+	//return 0;
+
+	//int ftemp;
+	//cout << "Enter temperature in fahrenheit: ";
+	//cin >> ftemp;
+	//int ctemp = (ftemp - 32) * 5 / 9;
+	//cout << "Equivalent in Celsius is: " << ctemp << '\n';
+	//_getch();
+	//return 0;
+
+	float rad;
+	const float PI = 3.14159F;
+	cout << "Enter radius of circle: ";
+	cin >> rad;
+	float area = PI * rad * rad;
+	cout << "Area is " << area << endl;
+	_getch();
+	return 0;
+
+	
+}
